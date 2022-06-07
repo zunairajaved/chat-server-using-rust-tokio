db5f70e0d4987c1417b3725b5d7d57f767fc6483
zunairajaved <zunaira.javed@invozone.com>
2022-06-07 16:24:59 +0500

    chat server using rust and tokio

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use tokio::{
+    io::{BufReader,AsyncBufReadExt,AsyncWriteExt},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
+
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx ,_rx) = broadcast::channel(10);
+    loop {
+
+    let (mut socket , _addr) = listener.accept().await.unwrap();
+    let tx = tx.clone();
+    let mut rx = tx.subscribe();
+
+    tokio::spawn(async move{
+        let (reader ,mut writer )= socket.split();
+
+        let mut reader = BufReader::new(reader);
+        let mut line = String::new();
+    loop {
+        tokio::select! {
+            result= reader.read_line(&mut line) =>{
+                if result.unwrap() == 0 {
+                break;
+                }
+
+                tx.send((line.clone(),_addr)).unwrap();
+                line.clear();
+            }
+        result = rx.recv() => {
+            let (msg , other_addr) = result.unwrap();
+            if _addr != other_addr{
+                writer.write_all(msg.as_bytes()).await.unwrap();
+            }
+        }
+       
+    }
+}
+});
+
+    // println!("Hello, world!");
+}
+}
